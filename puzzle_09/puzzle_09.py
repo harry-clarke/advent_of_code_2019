f39a7ef30e946d21fe0cafddd91b1fc4438ee3c9
Harry Clarke <harry-clarke@outlook.com>
2019-12-09 23:43:57 +0000

    Puzzle 9, Answer 1

---
+++ puzzle_09/puzzle_09.py
@@ -0,0 +1,18 @@
+from util.intcode import IntCode, run_as_function
+
+
+def read_tape():
+    with open('input.txt') as f:
+        s = f.readline()
+        return list(map(int, s.split(',')))
+
+
+TAPE = read_tape()
+
+
+def answer_1():
+    r = run_as_function(TAPE, [1])
+    print('Answer 1: %s' % r)
+
+if __name__ == '__main__':
+    answer_1()
