6e177bfaac14b05822a19f320946de60f18ddbb2
Harry Clarke <harry-clarke@outlook.com>
2019-12-22 18:35:31 +0000

    Puzzle 10, Answer 1

--- puzzle_10/puzzle_10.py
+++ puzzle_10/puzzle_10.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from functools import reduce
-from math import pi, hypot, atan, copysign, degrees, atan2
+from math import pi, hypot, atan, copysign, degrees, atan2, floor
 from typing import Callable, Union, List, Tuple, Optional
 
 TEST_1 = (
@@ -8,18 +8,45 @@ TEST_1 = (
     ['.7..7', '.....', '67775', '....7', '...87']
 )
 
+TEST_2 = [
+    (
+        ['......#.#.', '#..#.#....', '..#######.', '.#.#.###..', '.#..#.....', '..#....#.#', '#..#....#.', '.##.#..###',
+         '##...#..#.', '.#....####'],
+        ((5, 8), 33)
+    ),
+    (
+        ['#.#...#.#.', '.###....#.', '.#....#...', '##.#.#.#.#', '....#.#.#.', '.##..###.#', '..#...##..', '..##....##',
+         '......#...', '.####.###.'],
+        ((1, 2), 35)
+    ),
+    (
+        ['.#..#..###', '####.###.#', '....###.#.', '..###.##.#', '##.##.#.#.', '....###..#', '..#.#..#.#', '#..#.#.###',
+         '.##...##.#', '.....#.#..'],
+        ((6, 3), 41)
+    ),
+    (
+        ['.#..##.###...#######', '##.############..##.', '.#.######.########.#', '.###.#######.####.#.',
+         '#####.##.#.##.###.##', '..#####..#.#########', '####################', '#.####....###.#.#.##',
+         '##.#################', '#####.##.###..####..', '..######..##.#######', '####.##.####...##..#',
+         '.#####..#.######.###', '##...#.##########...', '#.##########.#######', '.####.#.###.###.#.##',
+         '....##.##.###..#####', '.#.#.###########.###', '#.#.#.#####.####.###', '###.##.####.##.#..##'],
+        ((11, 13), 210)
+    )
+]
+
+COORD = Tuple[int, int]
+DISTANCE = float
+BEARING = float
+BEARING_DECIMAL_PLACES = 2
+# Where a given coordinate is in relation to the current object
+RELATIVE_LOCATION = {COORD: (BEARING, DISTANCE)}
+RELATIVE_LOCATIONS = {COORD: RELATIVE_LOCATION}
+# What is in the immediate line of sight for an object at a given bearing
+LINE_OF_SIGHT = {BEARING: COORD}
+LINES_OF_SIGHT = {COORD: LINE_OF_SIGHT}
+
 
 class AsteroidField:
-    COORD = Tuple[int, int]
-    DISTANCE = float
-    BEARING = float
-    # Where a given coordinate is in relation to the current object
-    RELATIVE_LOCATION = {COORD: (BEARING, DISTANCE)}
-    RELATIVE_LOCATIONS = {COORD: RELATIVE_LOCATION}
-    # What is in the immediate line of sight for an object at a given bearing
-    LINE_OF_SIGHT = {BEARING: COORD}
-    LINES_OF_SIGHT = {COORD: LINE_OF_SIGHT}
-
     asteroids: {COORD}
     # Only maps asteroids that are reachable through a line of sight
     asteroid_map: RELATIVE_LOCATIONS
@@ -29,8 +56,9 @@ class AsteroidField:
             asteroids = AsteroidField.__parse_asteroid_field(asteroids)
         self.asteroids = set(asteroids)
         self.asteroid_map = {}
+        self.__calc_all_lines_of_site()
 
-    def calc_all_lines_of_site(self) -> LINES_OF_SIGHT:
+    def __calc_all_lines_of_site(self) -> LINES_OF_SIGHT:
         for coords in self.asteroids:
             asteroid = Asteroid(self, coords)
             asteroid.calc_lines_of_sight()
@@ -72,13 +100,14 @@ class AsteroidField:
 
 
 class Asteroid:
-    def __init__(self, asteroid_field: AsteroidField, coords: AsteroidField.COORD):
+
+    def __init__(self, asteroid_field: AsteroidField, coords: COORD):
         self.asteroid_field = asteroid_field
         self.coords = coords
         self.res = {}
-        self.los: AsteroidField.LINE_OF_SIGHT = {}
+        self.los: LINE_OF_SIGHT = {}
 
-    def calc_lines_of_sight(self) -> AsteroidField.LINE_OF_SIGHT:
+    def calc_lines_of_sight(self) -> LINE_OF_SIGHT:
         if self.coords in self.asteroid_field.asteroid_map:
             return self.asteroid_field.asteroid_map[self.coords]
 
@@ -94,12 +123,12 @@ class Asteroid:
         self.asteroid_field.asteroid_map[self.coords] = self.res
         return self.res
 
-    def calc_line_of_sight(self: Asteroid, remote: AsteroidField.COORD) -> Optional[
-            Tuple[AsteroidField.BEARING, AsteroidField.DISTANCE]]:
+    def calc_line_of_sight(self: Asteroid, remote: COORD) -> Optional[
+        Tuple[BEARING, DISTANCE]]:
         if remote in self.asteroid_field.asteroid_map:
             if self.coords in self.asteroid_field.asteroid_map[remote]:
                 recip_bearing, distance = self.asteroid_field.asteroid_map[remote][self.coords]
-                bearing = (recip_bearing + 180) % 360
+                bearing = round((recip_bearing + 180) % 360, BEARING_DECIMAL_PLACES)
                 return bearing, distance
             else:
                 return
@@ -114,24 +143,45 @@ class Asteroid:
                 return bearing, distance
 
     @staticmethod
-    def __calc_los(a1: AsteroidField.COORD, a2: AsteroidField.COORD) -> (AsteroidField.BEARING, AsteroidField.DISTANCE):
+    def __calc_los(a1: COORD, a2: COORD) -> (BEARING, DISTANCE):
         (x1, y1), (x2, y2) = a1, a2
         dx, dy = x2 - x1, y1 - y2
         dist = hypot(dx, dy)
-        bearing = int((90 - degrees(atan2(dy, dx))) % 360)
+        # bearing = int((90 - degrees(atan2(dy, dx))) % 360)
+        bearing = round((90 - degrees(atan2(dy, dx))) % 360, BEARING_DECIMAL_PLACES)
         return bearing, dist
+
+
+def get_best_asteroid(af: AsteroidField):
+    asteroids = sorted(af.asteroid_map.items(), key=lambda kv: len(kv[1]), reverse=True)
+    return asteroids[0][0]
 
 
 def test_1():
     af = AsteroidField(TEST_1[0])
-    af.calc_all_lines_of_site()
-    # los = af.calc_asteroid_los((1, 0))
-    # print(af.asteroid_map[(1, 0)])
-    # for l in los:
-    #     print(l)
     print(af.show_field_los())
     # print(af)
 
 
+def test_2():
+    for t in TEST_2:
+        af = AsteroidField(t[0])
+        a = get_best_asteroid(af)
+        l = len(af.asteroid_map[a])
+        assert a == t[1][0]
+        assert l == t[1][1]
+
+
+def answer_1():
+    with open('input.txt') as f:
+        s = f.readlines()
+    af = AsteroidField(s)
+    a = get_best_asteroid(af)
+    l = len(af.asteroid_map[a])
+    print('Answer 1: %s (%s asteroids)' % (a, l))
+
+
 if __name__ == '__main__':
     test_1()
+    test_2()
+    answer_1()
