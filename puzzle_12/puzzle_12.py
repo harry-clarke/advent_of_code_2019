be9b7c3d33f7cbc2b1e7b4b0bd743eeb7731ef1f
Harry Clarke <harry-clarke@outlook.com>
2019-12-23 21:27:26 +0000

    WIP

--- puzzle_12/puzzle_12.py
+++ puzzle_12/puzzle_12.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+import sys
+from typing import Tuple
+
+import humanfriendly
+import time
 from math import copysign
 
 INPUT = (
@@ -18,10 +23,10 @@ TEST = (
 
 
 class Jupiter:
-    moons: list[Moon]
+    moons: Tuple[Moon]
 
-    def __init__(self, moons: set[tuple[int]]):
-        self.moons = list([Moon(self, pos) for pos in moons])
+    def __init__(self, moons):
+        self.moons = tuple([Moon(self, pos) for pos in moons])
 
     def step(self):
         for moon in self.moons:
@@ -32,19 +37,22 @@ class Jupiter:
     def get_total_energy(self):
         return sum(moon.get_total_energy() for moon in self.moons)
 
+    def get_state(self):
+        return tuple(m.get_state() for m in self.moons)
+
     def __str__(self):
         return str(self.moons)
 
 
 class Moon:
     jupiter: Jupiter
-    pos: [int]
-    vel: [int]
+    pos: Tuple[int]
+    vel: Tuple[int]
 
     def __init__(self, jupiter, pos):
         self.jupiter = jupiter
         self.pos = pos
-        self.vel = [0 for _ in pos]
+        self.vel = tuple(0 for _ in pos)
 
     def get_total_energy(self):
         return self.get_potential_energy() * self.get_kinetic_energy()
@@ -56,16 +64,19 @@ class Moon:
         return sum(map(abs, self.vel))
 
     def velocity_step(self):
-        self.vel = [
-            vel_n + sum([
-                0 if (diff := moon.pos[n] - self.pos[n]) == 0 else copysign(1, diff)
+        self.vel = tuple(
+            vel_n + sum(
+                0 if (diff := moon.pos[n] - self.pos[n]) == 0 else int(copysign(1, diff))
                 for moon in self.jupiter.moons
-            ])
+            )
             for n, vel_n in enumerate(self.vel)
-        ]
+        )
 
     def position_step(self):
-        self.pos = [p + v for p, v in zip(self.pos, self.vel)]
+        self.pos = tuple(p + v for p, v in zip(self.pos, self.vel))
+
+    def get_state(self):
+        return self.pos, self.vel
 
     def __repr__(self):
         return self.__str__()
@@ -90,6 +101,45 @@ def answer_1():
     print(f'Answer 1: {j.get_total_energy()}')
 
 
+def find_dimensional_orbit(j, n):
+    def map_state(s):
+        return [[v[n] for v in m] for m in s]
+    initial_state = map_state(j.get_state())
+    i = 1
+    it = int(time.perf_counter())
+    ct = it
+    j.step()
+    while map_state(j.get_state()) != initial_state:
+        i += 1
+        j.step()
+        if time.perf_counter() - ct >= 5:
+            ct = int(time.perf_counter())
+            print(
+                f'@{ct - it}s: {i} steps ({i / ct - it}/s)'
+            )
+    return i, j
+
+
+def find_complete_orbit(j):
+    initial_state = j.get_state()
+
+
+
+def test_2():
+    j = Jupiter(TEST)
+    i, j = find_complete_orbit(j)
+    assert 2772 == i
+    print(f'Answer 2, T1: {i} ({j}')
+
+
+def answer_2():
+    j = Jupiter(INPUT)
+    i, j = find_complete_orbit(j)
+    print(f'Answer 2: {i} ({j})')
+
+
 if __name__ == '__main__':
     test_1()
     answer_1()
+    test_2()
+    answer_2()
